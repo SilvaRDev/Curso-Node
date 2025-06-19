5525965e3a80d6264b46a128821e04cc5a8b9dc0
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-19 09:13:14 -0300

    aula 6 finalizada.

---
+++ 4-NPM-FUNDAMENTAL/6-pacote-global/index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk')
+
+const corCiano = '#13d1cb'
+const corVioleta = '#4630c7'
+
+console.log(chalk.hex(corCiano)('Teste'))
+console.log(chalk.hex(corVioleta)('Testando'))
