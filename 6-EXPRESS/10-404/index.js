b7fb10be6a2f56b177437595b75d8434cbb88d12
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-23 13:17:15 -0300

    Aula de inserção de 404 finalizada.

---
+++ 6-EXPRESS/10-404/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path')
+
+const users = require('./users') // Importando o router
+
+// Ler o body
+app.use( // Converte os dados do form em um objeto. Sendo possível acessá-lo pelo req.body
+  express.urlencoded({  
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+// Arquivos estáticos
+app.use(express.static('public'))
+
+const basePath = path.join(__dirname, 'templates') // Acessa o diretório onde nossos html estão.
+
+app.use('/users', users) // Exporta as rotas presentes no módulo users
+
+app.get('/', (req, res) => {
+  res.sendFile(`${basePath}/index.html`) // Acessa o arquivo que será vinculado a rota
+})
+
+app.use(function(req, res, next) {
+  res.status(404).sendFile(`${basePath}/404.html`)
+})
+
+app.listen(port, () => {
+  console.log(`Aplicação iniciada na porta ${port}!`)
+})
