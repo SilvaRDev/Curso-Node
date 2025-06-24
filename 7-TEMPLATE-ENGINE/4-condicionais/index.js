068a22643286c0eae989f70d03d0477571c75e90
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-24 10:34:24 -0300

    Aula 04 finalizada
    Aula sobre condicionais usando Handlebars finalizada.

---
+++ 7-TEMPLATE-ENGINE/4-condicionais/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+ 
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars') // Seta a modo de visualização da aplicação para o handlebars
+app.set('views', './views')
+
+const user = {
+  name: "Rafael",
+  surname: "Silva",
+  age: 19
+}
+
+const frase = "Testando alguma coisa!"
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard', { user: user })
+})
+
+app.get('/', (req, res) => {
+
+  const auth = true
+
+  res.render('home', { user: user, frase, auth }) // Por meio desse método, os dados são enviados do back para a view do front com auxílio do Handlebars.
+})
+
+app.listen(3000, () => {
+  console.log('App funcionando na porta 3000!')
+})
