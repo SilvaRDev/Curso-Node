135753f03847d6f5d9e6d6182999648300fa0357
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-24 09:30:23 -0300

    Aula 01 finalizada.
    Aula 01 do módulo 7, sobre instalação do Express-Handlebars finalizada.

---
+++ 7-TEMPLATE-ENGINE/1-instalacao-handlebars/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+ 
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars') // Seta a modo de visualização da aplicação para o handlebars
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+  res.render('home', { layout: false })
+})
+
+app.listen(3000, () => {
+  console.log('App funcionando na porta 3000!')
+})
