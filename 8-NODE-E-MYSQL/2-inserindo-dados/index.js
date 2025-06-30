7a19e3fbe4218b848078e614b7124bde0b2d7c4d
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-06-30 12:03:59 -0300

    Aula 02 finalizada

--- 8-NODE-E-MYSQL/2-inserindo-dados/index.js
+++ 8-NODE-E-MYSQL/2-inserindo-dados/index.js
@@ -5,6 +5,15 @@ const mysql = require('mysql')
 const app = express()
 const port = 3000
 
+/* INCORPORANDO REQUISIÇÕES DO BODY */
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+
+app.use(express.json())
+
 /* HANDLEBARS SETTINGS */
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -14,6 +23,25 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.post('/books/insertbook', (req, res) => {
+
+  const title = req.body.title
+  const pagesqty = req.body.pagesqty
+
+  const sql = `INSERT INTO books (title, pageqty) VALUES ('${title}', '${pagesqty}')`
+
+  conn.query(sql, (err) => {
+
+    if(err) {
+      console.log(err)
+    }
+
+    res.redirect('/')
+
+  })
+
 })
 
 const conn = mysql.createConnection({
