19a3c951abcf134edbaab01a90ce4b5bce307493
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-04 13:40:00 -0300

    Aula 08 finalizada.

---
+++ 10-MVC/8-removendo-dados/routes/tasksRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const TaskController = require('../controllers/TaskController')
+
+router.get('/add', TaskController.createTask) // Rotas que vão após o /tasks. Importado no Index via app.use
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/', TaskController.showTasks)
+
+module.exports = router
