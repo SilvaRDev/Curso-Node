cda12de7b4c496302b32101aece3e87db01474cf
Rafael Silva <104386903+SilvaRDev@users.noreply.github.com>
2025-07-15 17:15:09 -0300

    Aula 01 finalizada.

---
+++ 14-API/1-primeira-api/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+app.use(
+  express.urlencoded({
+    extended: true 
+  })
+)
+
+app.use(express.json())
+
+// rotas - endpoints
+app.get('/', (req, res) => {
+
+  res.json({message: "Olá, mundo!"}) // Enviamos um arquivo JSON...
+
+})
+
+app.listen(port)
