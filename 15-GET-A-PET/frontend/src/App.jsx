53e8210e4a1ee16945eee5b1982ebb5b68b1fa65
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-20 15:00:14 -0300

    Contexto de usuário criado.

--- 15-GET-A-PET/frontend/src/App.jsx
+++ 15-GET-A-PET/frontend/src/App.jsx
@@ -10,19 +10,24 @@ import Login from './pages/Auth/Login'
 import Register from './pages/Auth/Register'
 import Home from './pages/Home'
 
+/*  CONTEXTS */
+import { UserProvider } from './context/userContext'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Container>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Container>
-        <Footer />
+        <UserProvider>
+          <Navbar />
+          <Container>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
+          <Footer />
+        </UserProvider>
       </BrowserRouter>
     </div>
   )
