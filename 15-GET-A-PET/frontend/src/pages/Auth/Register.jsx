c5e29a9ade1c3a463b0bcb19848d8b4ec804b16e
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-20 12:00:32 -0300

    Inputs do form adicionados.

--- 15-GET-A-PET/frontend/src/pages/Auth/Register.jsx
+++ 15-GET-A-PET/frontend/src/pages/Auth/Register.jsx
@@ -1,7 +1,49 @@
+import Input from '../../components/form/Input'
+
 const Register = () => {
+  function handleChange(e) {}
+
   return (
     <section>
       <h1>Register</h1>
+      <form>
+        <Input
+          type="text"
+          text="Nome"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          type="text"
+          text="Telefone"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          type="email"
+          text="E-mail"
+          name="email"
+          placeholder="Digite o seu E-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          type="password"
+          text="senha"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          type="password"
+          text="Confirmação de senha"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Cadastrar" />
+      </form>
     </section>
   )
 }
