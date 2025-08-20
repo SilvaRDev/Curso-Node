c38de3abcdb7d484d7a784c3e5f3f14d87c70fce
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-20 11:39:52 -0300

    Container adicionado.

--- 15-GET-A-PET/frontend/src/App.jsx
+++ 15-GET-A-PET/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 /* COMPONENTS */
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Container from './components/layout/Container'
 
 /* PAGES */
 import Login from './pages/Auth/Login'
@@ -14,11 +15,13 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
         <Footer />
       </BrowserRouter>
     </div>
