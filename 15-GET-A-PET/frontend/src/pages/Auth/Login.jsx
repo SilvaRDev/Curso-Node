98324031a8561e7735db70e0432bec1ee6b91405
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-21 10:00:29 -0300

    Update Login.jsx

--- 15-GET-A-PET/frontend/src/pages/Auth/Login.jsx
+++ 15-GET-A-PET/frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,42 @@
+import { useState, useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+import Input from '../../components/form/Input'
+
+import styles from '../../components/form/Form.module.css'
+
+/* CONTEXT */
+import { Context } from '../../context/userContext'
+
 const Login = () => {
+
+  function handleChange(e) {
+    
+  }
+
   return (
-    <section>
+    <section className={styles.form_container}>
       <h1>Login</h1>
+      <form>
+        <Input 
+          type="email"
+          text="E-mail"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          type="senha"
+          text="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>
+        Não tem conta: <Link to='/register'>Clique Aqui</Link>
+      </p>
     </section>
   )
 }
