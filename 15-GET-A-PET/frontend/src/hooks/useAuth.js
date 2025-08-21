128605fdc139f38f1c391f39a5dc0d6fcc71b80d
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-21 10:21:01 -0300

    Sistema de login concluído

--- 15-GET-A-PET/frontend/src/hooks/useAuth.js
+++ 15-GET-A-PET/frontend/src/hooks/useAuth.js
@@ -36,13 +36,34 @@ export default function useAuth() {
       let msgType = 'error'
       setFlashMessage(msgText, msgType)
     }
+  }
 
-    async function authUser(data) {
-      setAuthenticated(true)
+  async function login(user) {
+    let msgText = 'Login realizado com sucesso!'
+    let msgType = 'success'
 
-      localStorage.setItem('token', JSON.stringify(data.token))
-      navigate('/')
+    try {
+
+      const data = await api.post('/users/login', user).then((response) => {
+        return response.data
+      })
+
+      await authUser(data)
+
+    } catch(error) {
+
+      msgText = error.response.data.message 
+      msgType = 'error'
+      setFlashMessage(msgText, msgType)
+
     }
+  }
+
+  async function authUser(data) {
+    setAuthenticated(true)
+
+    localStorage.setItem('token', JSON.stringify(data.token))
+    navigate('/')
   }
 
   function logout() {
@@ -57,5 +78,5 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType)
   }
 
-  return { authenticated, register, logout }
+  return { authenticated, register, logout, login }
 }
--- 15-GET-A-PET/frontend/src/pages/Auth/Login.jsx
+++ 15-GET-A-PET/frontend/src/pages/Auth/Login.jsx
@@ -9,25 +9,33 @@ import styles from '../../components/form/Form.module.css'
 import { Context } from '../../context/userContext'
 
 const Login = () => {
+  const [user, setUser] = useState({})
+  const { login } = useContext(Context)
 
   function handleChange(e) {
-    
+    setUser({ ...user, [e.target.name]: e.target.value })
+    console.log(user)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    login(user)
   }
 
   return (
     <section className={styles.form_container}>
       <h1>Login</h1>
-      <form>
-        <Input 
+      <form onSubmit={handleSubmit}>
+        <Input
           type="email"
           text="E-mail"
           name="email"
           placeholder="Digite o seu e-mail"
           handleOnChange={handleChange}
         />
-        <Input 
-          type="senha"
-          text="password"
+        <Input
+          type="password"
+          text="senha"
           name="password"
           placeholder="Digite a sua senha"
           handleOnChange={handleChange}
@@ -35,7 +43,7 @@ const Login = () => {
         <input type="submit" value="Entrar" />
       </form>
       <p>
-        Não tem conta: <Link to='/register'>Clique Aqui</Link>
+        Não tem conta: <Link to="/register">Clique Aqui</Link>
       </p>
     </section>
   )
