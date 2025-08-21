5e112d5d63f92926c5ae62c11d15fbbb64a53918
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-21 12:15:50 -0300

    Update Profile.jsx

--- 15-GET-A-PET/frontend/src/pages/User/Profile/Profile.jsx
+++ 15-GET-A-PET/frontend/src/pages/User/Profile/Profile.jsx
@@ -7,9 +7,12 @@ import formStyles from '../../../components/form/Form.module.css'
 
 import Input from '../../../components/form/Input'
 
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
 const Profile = () => {
   const [user, setUser] = useState([])
   const storedToken = localStorage.getItem('token')
+  const { setFlashMessage } = useFlashMessage()
 
   useEffect(() => {
     api
@@ -23,9 +26,41 @@ const Profile = () => {
       })
   }, [storedToken])
 
-  function onFileChange(e) {}
+  function onFileChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.files[0] })
+  }
 
-  function handleChange(e) {}
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    let msgType = 'success'
+
+    const formData = new FormData()
+
+    await Object.keys(user).forEach((key) => {
+      formData.append(key, user[key])
+    })
+
+    const data = await api
+      .patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(storedToken)}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((response) => {
+        return response.data
+      })
+      .catch((err) => {
+        msgType = 'error'
+        return err.response.data
+      })
+      setFlashMessage(data.message, msgType)
+    }
 
   return (
     <div>
@@ -33,7 +68,7 @@ const Profile = () => {
         <h1>Perfil</h1>
         <p>Preview Imagem</p>
       </div>
-      <form className={formStyles.form_container}>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input
           type="file"
           text="imagem"
