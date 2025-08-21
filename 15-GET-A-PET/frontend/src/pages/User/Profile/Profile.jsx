6e15afd55dbabc4654f1c4c6f60b4d8809bb4476
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-21 11:35:27 -0300

    Adicionado form de update do user

--- 15-GET-A-PET/frontend/src/pages/User/Profile/Profile.jsx
+++ 15-GET-A-PET/frontend/src/pages/User/Profile/Profile.jsx
@@ -1,9 +1,74 @@
+import { useState, useEffect } from 'react'
+
 import styles from './Profile.module.css'
+import formStyles from '../../../components/form/Form.module.css'
+
+import Input from '../../../components/form/Input'
 
 const Profile = () => {
+  const [user, setUser] = useState([])
+
+  function onFileChange(e) {
+
+  }
+  
+  function handleChange(e) {
+
+  }
+
   return (
-    <div>
-      <h1>Profile</h1>
+    <div >
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>Preview Imagem</p>
+      </div>
+      <form className={formStyles.form_container}>
+        <Input 
+          type="file"
+          text="imagem"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input 
+          type="email"
+          text="E-mail"
+          name="email"
+          placeholder="Digite o seu E-mail"
+          handleOnChange={handleChange}
+          value={user.email || ''}
+        />
+        <Input 
+          type="text"
+          text="Nome"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ''}
+        />
+        <Input 
+          type="text"
+          text="Telefone"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ''}
+        />
+        <Input 
+          type="password"
+          text="Senha"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input 
+          type="password"
+          text="Senha"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar" />
+      </form>
     </div>
   )
 }
