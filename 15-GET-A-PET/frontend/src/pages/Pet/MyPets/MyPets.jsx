6af44e66b26c5b07e43359dd825865a88d5825a7
SilvaRDev <rafaelfsilva605@gmail.com>
2025-08-22 11:38:59 -0300

    Dashboard dos pets adicionada

--- 15-GET-A-PET/frontend/src/pages/Pet/MyPets/MyPets.jsx
+++ 15-GET-A-PET/frontend/src/pages/Pet/MyPets/MyPets.jsx
@@ -3,7 +3,7 @@ import api from '../../../utils/api'
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
-import styles from './MyPets.module.css'
+import styles from '../Dashboard.module.css'
 
 /* Hook */
 import useFlashMessage from '../../../hooks/useFlashMessage'
@@ -34,7 +34,31 @@ const MyPets = () => {
         <Link to='/pet/add'>Cadastrar Pet</Link>
       </div>
       <div>
-        {pets.length > 0 && <p>Meus pets cadastrados</p>}
+        {pets.length > 0 && (
+          pets.map((pet) => (
+            <div key={pet._id}>
+              <RoundedImage 
+                src={`${import.meta.env.VITE_APP_API}/images/pets/${(pet.images[0])}`}
+                alt={pet.name}
+                width="75px"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.action}>
+                {pet.available ? ( 
+                  <>
+                    {pet.adopter && (
+                      <button>Concluir adoção</button>
+                    )}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet já adotado.</p>
+                )}
+              </div>
+            </div>
+          ))
+        )}
         {pets.length === 0 && <p>Não há pets cadastrados.</p> }
       </div>
     </section>
