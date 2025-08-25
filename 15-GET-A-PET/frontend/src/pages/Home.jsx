9637892d5defcc0e96e2e86799544a634107da85
cobaiadoraff <rafaelfsilva605@gmail.com>
2025-08-25 08:35:28 -0300

    Estrutura da Home adicionada.

--- 15-GET-A-PET/frontend/src/pages/Home.jsx
+++ 15-GET-A-PET/frontend/src/pages/Home.jsx
@@ -9,15 +9,34 @@ const Home = () => {
   const [pets, setPets] = useState([])
 
   useEffect(() => {
-    api.get('/pets')
-    .then((response) => {
+    api.get('/pets').then((response) => {
       setPets(response.data.pets)
     })
   }, [])
 
   return (
     <section>
-      <h1>Home</h1>
+      <div>
+        <h1>Adote um Pet</h1>
+        <p>Veja os detalhes de cada um e conheça o tutor deles</p>
+        <div>
+          {pets &&
+            pets.length > 0 &&
+            pets.map((pet) => (
+              <div key={pet._id}>
+                <p>Imagem do pet</p>
+                <h3>{pet.name}</h3>
+                <p>
+                  <span className="bold">Peso: {pet.weight}Kg</span>
+                </p>
+                {pet.available ? <Link to={`/pet/${pet._id}`}>Mais detalhes</Link> : <p>Adotado</p>}
+              </div>
+            ))}
+          {pets && pets.length === 0 && (
+            <p>Não há pets disponíveis para adoção no momento.</p>
+          )}
+        </div>
+      </div>
     </section>
   )
 }
