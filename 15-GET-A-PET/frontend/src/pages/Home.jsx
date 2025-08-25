aaabe772978ca23f75f90c572f6795b6af503803
cobaiadoraff <rafaelfsilva605@gmail.com>
2025-08-25 08:55:00 -0300

    Estilo da Home adicionado.

--- 15-GET-A-PET/frontend/src/pages/Home.jsx
+++ 15-GET-A-PET/frontend/src/pages/Home.jsx
@@ -16,26 +16,28 @@ const Home = () => {
 
   return (
     <section>
-      <div>
+      <div className={styles.pet_home_header}>
         <h1>Adote um Pet</h1>
         <p>Veja os detalhes de cada um e conheça o tutor deles</p>
-        <div>
-          {pets &&
-            pets.length > 0 &&
-            pets.map((pet) => (
-              <div key={pet._id}>
-                <p>Imagem do pet</p>
-                <h3>{pet.name}</h3>
-                <p>
-                  <span className="bold">Peso: {pet.weight}Kg</span>
-                </p>
-                {pet.available ? <Link to={`/pet/${pet._id}`}>Mais detalhes</Link> : <p>Adotado</p>}
+      </div>
+      <div className={styles.pet_container}>
+        {pets &&
+          pets.length > 0 &&
+          pets.map((pet) => (
+            <div className={styles.pet_card} key={pet._id}>
+              <div style={{backgroundImage: `url(${import.meta.env.VITE_APP_API}/images/pets/${pet.images[0]})`}} className={styles.pet_card_image}>
+
               </div>
-            ))}
-          {pets && pets.length === 0 && (
-            <p>Não há pets disponíveis para adoção no momento.</p>
-          )}
-        </div>
+              <h3>{pet.name}</h3>
+              <p>
+                <span className="bold">Peso: {pet.weight}Kg</span>
+              </p>
+              {pet.available ? <Link to={`/pet/${pet._id}`}>Mais detalhes</Link> : <p className={styles.adopted_text}>Adotado</p>}
+            </div>
+          ))}
+        {pets && pets.length === 0 && (
+          <p>Não há pets disponíveis para adoção no momento.</p>
+        )}
       </div>
     </section>
   )
